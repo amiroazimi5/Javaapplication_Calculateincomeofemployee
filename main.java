4365624b8d4dfaf07e36965e40cf043f9391d68c
amirhossein azim nasab <111241553+amiroazimi5@users.noreply.github.com>
2024-11-19 12:10:33 +0330

    Version 1.5

--- main.java
+++ main.java
@@ -1,21 +1,35 @@
 import java.util.*;
 
-public class main {
-    public static void main(String[]args) {
-        
-        System.out.print("Enter Number Of employee : ");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter Number Of employee: ");
         Scanner sc = new Scanner(System.in); // Create Scanner
+
         int b = sc.nextInt();
+        sc.nextLine();  // Consume the newline character
+
         String[] names = new String[b]; // Create list for names
         int[] numbers = new int[b]; // Create list for numbers
-        System.out.println("enter name of emplyee and income.");
-        for (int i = 0; i < b; i++)names[i] = sc.nextLine();
-        for (int i = 0; i < b; i++)numbers[i] = sc.nextInt();
+
+        System.out.println("Enter name of employee and income:");
+
+
+        for (int i = 0; i < b; i++) {
+            System.out.print("Employee " + (i + 1) + " name: ");
+            names[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < b; i++) {
+            System.out.print("Employee " + (i + 1) + " income: ");
+            numbers[i] = sc.nextInt();
+        }
+
         sc.close(); // Close Scanner
 
         for (int i = 0; i < b; i++) {
-            System.out.print((i + 1) + "- " + names[i] + " Length : " + names[i].length() + " ");
-            System.out.println("income : " + (numbers[i] >= 5700000 ? numbers[i] * 1.0165 : numbers[i]));
+            System.out.print((i + 1) + "- " + names[i] + " Length: " + names[i].length() + " ");
+            System.out.println("income: " + (numbers[i] >= 5700000 ? numbers[i] * 1.0165 : numbers[i]));
         }
     }
 }
