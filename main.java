ec8933440df93c3089e48d2b10b43553d8012d5e
amirhossein azim nasab <111241553+amiroazimi5@users.noreply.github.com>
2024-11-19 11:51:30 +0330

    Version 1.4

--- main.java
+++ main.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
 public class main {
-    public static void main(String[] args) {
-        //Declares Variables
-        String[] names = new String[10];
-        int[] numbers = new int[10];
-
-        Scanner sc = new Scanner(System.in);
-
-        //get input from user.
-        for (int i = 0; i < 10; i++)names[i] = sc.nextLine();
-        for (int i = 0; i < 10; i++)numbers[i] = sc.nextInt();
-        sc.close(); // Close Scanner
+    public static void main(String[]args) {
         
-        // Output information
-        for (int i = 0; i < 10; i++) {
+        System.out.print("Enter Number Of employee : ");
+        Scanner sc = new Scanner(System.in); // Create Scanner
+        int b = sc.nextInt();
+        String[] names = new String[b]; // Create list for names
+        int[] numbers = new int[b]; // Create list for numbers
+        System.out.println("enter name of emplyee and income.");
+        for (int i = 0; i < b; i++)names[i] = sc.nextLine();
+        for (int i = 0; i < b; i++)numbers[i] = sc.nextInt();
+        sc.close(); // Close Scanner
+
+        for (int i = 0; i < b; i++) {
             System.out.print((i + 1) + "- " + names[i] + " Length : " + names[i].length() + " ");
             System.out.println("income : " + (numbers[i] >= 5700000 ? numbers[i] * 1.0165 : numbers[i]));
         }
